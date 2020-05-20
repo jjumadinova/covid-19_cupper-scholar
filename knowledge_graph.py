a2ef348b66069e3af928f43b68f16a6623b040a1
bagashvilit <bagashvilit@allegheny.edu>
2020-05-20 20:18:35 +0400

    Refactor some parts

---
+++ knowledge_graph.py
@@ -0,0 +1,4 @@
+
+#read in the generated text from the file
+with open('summary.txt', 'r') as file:
+    data = file.read()
--- summary_generator.py
+++ summary_generator.py
@@ -27,44 +27,45 @@ def summary_generator():
 
     s = ' '.join(filtered)
 
-    # Filter the data from extra symbols
-    article_text = re.sub(r'\[[0-9]*\]', ' ', s)
-    article_text = re.sub(r'\s+', ' ', s)
+    with open('summary.txt', 'r') as file:
+        s = file.read()
 
-    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+    # Filter the data from extra symbols
+    text = re.sub(r'\[[0-9]*\]', ' ', s)
+    text = re.sub(r'\s+', ' ', s)
+
+    formated_text = re.sub('[^a-zA-Z]', ' ',text )
+    formated_text = re.sub(r'\s+', ' ', formated_text)
 
     sentence_list = nltk.sent_tokenize(s)
 
     stopwords = nltk.corpus.stopwords.words('english')
 
     # Find the frequencies of the words
-    word_frequencies = {}
-    for word in nltk.word_tokenize(formatted_article_text):
+    word_freqs = {}
+    for word in nltk.word_tokenize(formated_text):
         if word not in stopwords:
             # make sure that the word is not stopword
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
+            if word not in word_freqs.keys():
+                word_freqs[word] = 1
             else:
-                word_frequencies[word] += 1
+                word_freqs[word] += 1
 
-    # compute the weighted frequencies.
-    maximum_frequncy = max(word_frequencies.values())
+    # compute the weighted frequencies, by dividing the frequencies of the words
+    # by the word that has highest frequency
+    for word in word_freqs.keys():
+        word_freqs[word] = (word_freqs[word]/max(word_freqs.values()))
 
-    for word in word_frequencies.keys():
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-
-    # Find the sentence scores for sentences that have less then 30 words. This
-    # value can be modified based on the personal preference.
+    # Find the sentence scores by adding the weighted frequencies of the words in each sentence.
     sentence_scores = {}
     for sent in sentence_list:
         for word in nltk.word_tokenize(sent.lower()):
-            if word in word_frequencies.keys():
-                if len(sent.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
+            if word in word_freqs.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_freqs[word]
+                else:
+                    sentence_scores[sent] += word_freqs[word]
 
 
     # retrieve 150 sentences with the highest scores.
