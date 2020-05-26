468e0edd7f0f9e4e9aaaee4d89fba2831f4ca50c
bagashvilit <bagashvilit@allegheny.edu>
2020-05-26 17:12:33 +0400

    Fix color and nodes

--- knowledge_graph.py
+++ knowledge_graph.py
@@ -18,10 +18,10 @@ print(pd.Series(relation).value_counts()[:50])
 
 kg_df = pd.DataFrame({'source':subject, 'target':object, 'edge':relation})
 
-G=nx.from_pandas_edgelist(kg_df[kg_df['edge']=="is in"], "source", "target",
+G=nx.from_pandas_edgelist(kg_df[kg_df['edge']=="is with"], "source", "target",
                           edge_attr=True, create_using=nx.MultiDiGraph())
 
 plt.figure(figsize=(16,16))
-pos = nx.spring_layout(G, k = 1) # k regulates the distance between nodes
-nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_cmap=plt.cm.Blues, pos = pos)
+pos = nx.spring_layout(G, k = 0.5) # k regulates the distance between nodes
+nx.draw(G, with_labels=True, node_color='green', node_size=1500, edge_cmap=plt.cm.Blues, pos = pos, font_size = 16)
 plt.show()
