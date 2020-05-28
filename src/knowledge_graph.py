de7373ad4d34558eb989db3879cd02d52ca1150c
bagashvilit <bagashvilit@allegheny.edu>
2020-05-28 18:34:16 +0400

    Add sample output

--- src/knowledge_graph.py
+++ src/knowledge_graph.py
@@ -2,8 +2,8 @@ import json
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import json
+import collections
 
 with open("../data/file.json") as f:
   data = json.load(f)
@@ -24,5 +24,8 @@ G=nx.from_pandas_edgelist(kg_df[kg_df['edge']=="is with"], "source", "target",
 plt.figure(figsize=(16,16))
 pos = nx.spring_layout(G, k = 0.5) # k regulates the distance between nodes
 nx.draw(G, with_labels=True, node_color='green', node_size=500, edge_cmap=plt.cm.Blues, pos = pos, font_size = 16)
+
+
+
 plt.savefig('../sample_outputs/sample.png')
 plt.show()
