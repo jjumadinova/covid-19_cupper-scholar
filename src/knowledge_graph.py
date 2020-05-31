10fd9ff0e9dfefb2d4d480e6765a41fadbced13c
bagashvilit <bagashvilit@allegheny.edu>
2020-05-31 18:37:56 +0400

    Add summary with its links

--- src/knowledge_graph.py
+++ src/knowledge_graph.py
@@ -4,9 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import collections
+import nltk
 
 with open("../data/file.json") as f:
   data = json.load(f)
+
+with open("../data/summary_urls.json") as f:
+  data_summary = json.load(f)
+
+summary_list = []
+for key,value in data_summary.items():
+    summary_list.append([key,"[link]","({})".format(value)])
+
+with open('../sample_outputs/summary.md', 'w') as f:
+    for i in summary_list:
+        string = ''.join(i)
+
+        f.write(string)
+        print(string)
 
 subject = (data["subject"])
 object = (data["object"])
@@ -18,16 +33,15 @@ print(pd.Series(relation).value_counts()[:10])
 
 kg_df = pd.DataFrame({'source':subject, 'target':object, 'edge':relation})
 
-relation = input("\nPlease enter one of the relations listed above\n")
 
-G=nx.from_pandas_edgelist(kg_df[kg_df['edge']==relation], "source", "target",
+relation_input = input("\nPlease enter one of the relations listed above\n")
+
+G=nx.from_pandas_edgelist(kg_df[kg_df['edge']==relation_input], "source", "target",
                           edge_attr=True, create_using=nx.MultiDiGraph())
 
 plt.figure(figsize=(16,16))
 pos = nx.spring_layout(G, k = 0.5) # k regulates the distance between nodes
 nx.draw(G, with_labels=True, node_color='green', node_size=500, edge_cmap=plt.cm.Blues, pos = pos, font_size = 16)
-
-
 
 plt.savefig('../sample_outputs/sample.png')
 plt.show()
--- src/summary_generator.py
+++ src/summary_generator.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import glob
-import os
 import nltk
 import re
+import heapq
+import json
+import itertools
 #nltk.download('stopwords')
 
 
@@ -29,21 +30,19 @@ links = []
 for i in meta_df['url']:
     links.append(i)
 
-print(len(filtered))
-print(len(links))
-
 s = ' '.join(filtered)
+#
+tokenized = []
+for i in filtered:
+    sent_list = nltk.sent_tokenize(i)
+    tokenized.append(sent_list)
+
 
 article_text = re.sub(r'\[[0-9]*\]', ' ', s)
 article_text = re.sub(r'\s+', ' ', s)
 
 formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
 formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-#
-tokenized = []
-for i in filtered:
-    sent_list = nltk.sent_tokenize(i)
-    tokenized.append(sent_list)
 
 
 data = dict(zip(links, tokenized))
@@ -52,35 +51,41 @@ data = dict(zip(links, tokenized))
 sentence_list = nltk.sent_tokenize(s)
 
 stopwords = nltk.corpus.stopwords.words('english')
-extra_words = ["Results","Methods","Abstract"]
 
-word_frequencies = {}
-for word in nltk.word_tokenize(formatted_article_text):
-    if word not in stopwords:
-        if word not in extra_words:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
+def word_frequencies(article):
+    word_freqs = {}
+    for word in nltk.word_tokenize(article):
+        if word not in stopwords:
+            if word not in word_freqs.keys():
+                word_freqs[word] = 1
             else:
-                word_frequencies[word] += 1
+                word_freqs[word] += 1
+    return word_freqs
 
-maximum_frequncy = max(word_frequencies.values())
+frequencies = word_frequencies(formatted_article_text)
 
-for word in word_frequencies.keys():
-    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+maximum_frequncy = max(frequencies.values())
+
+for word in frequencies.keys():
+    frequencies[word] = (frequencies[word]/maximum_frequncy)
+
+def sent_scores(sentences, word_freqs):
+    sentence_scores = {}
+    for sent in sentences:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_freqs.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_freqs[word]
+                    else:
+                        sentence_scores[sent] += word_freqs[word]
+    return sentence_scores
+
+scores = sent_scores(sentence_list, frequencies)
 
 
-sentence_scores = {}
-for sent in sentence_list:
-    for word in nltk.word_tokenize(sent.lower()):
-        if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
-                if sent not in sentence_scores.keys():
-                    sentence_scores[sent] = word_frequencies[word]
-                else:
-                    sentence_scores[sent] += word_frequencies[word]
 
-import heapq
-summary_sentences = heapq.nlargest(150, sentence_scores, key=sentence_scores.get)
+summary_sentences = heapq.nlargest(150, scores, key = scores.get)
 
 sent_link = {}
 for i in summary_sentences:
@@ -88,35 +93,10 @@ for i in summary_sentences:
         if i in value:
             sent_link.update({i:key})
 
-import json
-
-with open('../data/summary_urls.json', 'w') as outfile:
-    json.dump(sent_link, outfile, indent=4)
 
 
-print(sent_link)
-
-
-
+# with open('../data/summary_urls.json', 'w') as outfile:
+#     json.dump(sent_link, outfile, indent=4)
 print("here is summary")
 summary = ' '.join(summary_sentences)
 print(summary)
-
-
-
-#
-#
-# def is_empty(path):
-#     """Check if the path exitst and if file is empty"""
-#     return os.path.exists(path) and os.stat(path).st_size == 0
-#
-#
-# empty = is_empty('../data/summary.txt')
-#
-# if empty:
-#     # Open a file, if the file does not exist with mode `a` new one will be created
-#     file_object = open('../data/summary.txt', 'a')
-#     # add the generated string to the file, input is the file with subtitles
-#     file_object.write(summary)
-#     # Close the file
-#     file_object.close()
